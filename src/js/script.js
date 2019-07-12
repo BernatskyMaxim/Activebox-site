52d2ec9a8b88fde5f577dff8b3d8553d37e7e622
Maxim <maxsateon@gmail.com>
2019-07-12 09:45:38 +0300

    nav toggle

--- src/js/script.js
+++ src/js/script.js
@@ -5,6 +5,8 @@ $(function() {
     let intro = $('#intro');
     let introH = intro.innerHeight();
     let scrollPos = $(window).scrollTop();
+    let nav = $('#nav');
+    let navToggle = $('#navToggle');
     
     $(window).on('scroll load', function() {
         scrollPos = $(this).scrollTop();
@@ -23,10 +25,18 @@ $(function() {
         let elementId = $(this).data('scroll');
         let elementOffset = $(elementId).offset().top;
 
+        nav.removeClass('show');
+
         $('html, body').animate({
             scrollTop: elementOffset - 70
         }, 1000);
     });
 
+    // Nav Toggle
+    navToggle.on('click', function(event) {
+        event.preventDefault();
+
+        nav.toggleClass('show');
+    });   
     
 });
